86d8dbfe50ba29c8e8a8d993e0aa7a97ec424cfc
skleinjung <sean@thrashplay.com>
2024-02-01 02:40:36 -0600

    update lab-proxy-vm to provision via ansible webhook

--- modules/lab-proxy-vm/main.tf
+++ modules/lab-proxy-vm/main.tf
@@ -1,15 +1,16 @@
 locals {
   // extract useful global config values
-  assigned_ips        = module.config.ip_addresses
-  assigned_vmids      = module.config.vmids
-  config_repository   = module.config.config_repository
-  datastore_cloudinit = module.config.proxmox_datastore_hdd
-  datastore_snippets  = module.config.proxmox_datastore_snippets
-  datastore_ssd       = module.config.proxmox_datastore_ssd
-  iso_ids             = module.config.iso_ids
-  playbook_repository = module.config.github_playbook_repository
-  proxmox_node        = module.config.proxmox_default_node
-  all_users           = module.config.all_users
+  ansible_control_node_cert = module.config.ansible_control_node_cert
+  ansible_control_node_url  = module.config.ansible_control_node_url
+  assigned_ips              = module.config.ip_addresses
+  assigned_vmids            = module.config.vmids
+  config_repository         = module.config.config_repository
+  datastore_cloudinit       = module.config.proxmox_datastore_hdd
+  datastore_snippets        = module.config.proxmox_datastore_snippets
+  datastore_ssd             = module.config.proxmox_datastore_ssd
+  iso_ids                   = module.config.iso_ids
+  proxmox_node              = module.config.proxmox_default_node
+  all_users                 = module.config.all_users
 
   // module specific config
   boot_disk_datastore = local.datastore_ssd
@@ -33,36 +34,19 @@ module "config" {
 # nextcloud - cron broken
 # assign vmid without an ip?
 
-module "ansible" {
-  source = "github.com/pegasuspad/tf-modules.git//modules/cloudinit-ansible-auto-provision?ref=main"
-
-  host_name    = local.vm_name
-  repository   = local.playbook_repository
-  vault_secret = var.ansible_vault_secret
-
-  galaxy_collections = [
-    "ansible.posix:1.5.4",
-    "community.general:6.4.0"
-  ]
-
-  galaxy_roles = [
-    "nginxinc.nginx"
-  ]
-}
-
 module "virtual_machine" {
   source = "github.com/pegasuspad/tf-modules.git//modules/proxmox-virtual-machine?ref=main"
 
   boot_iso_id          = local.iso_ids.ubuntu_2204_20240126
   cloud_init_datastore = local.datastore_cloudinit
-  cloud_init_tasks     = [module.ansible.task]
   mac_address          = "aa:59:b5:53:b6:2f"
-  memory               = 2048
+  memory               = 512
   name                 = local.vm_name
   network_config       = local.network_config
   proxmox_node         = local.proxmox_node
   snippets_datastore   = local.datastore_snippets
-  tags                 = ["infrastructure", "lab"]
+  startup_phase        = "infrastructure"
+  tags                 = ["lab"]
   users                = local.users
   vmid                 = local.vmid
 
@@ -70,5 +54,16 @@ module "virtual_machine" {
     datastore = local.boot_disk_datastore
     size      = 32
     ssd       = local.boot_disk_datastore == local.datastore_ssd
+  }
+}
+
+# rerun Ansible provsioning whenever the revision ID of our VM changes
+resource "terraform_data" "trigger_ansible" {
+  triggers_replace = [ 
+    module.virtual_machine.revision_id 
+  ]
+
+  provisioner "local-exec" {
+    command = "curl -s --cacert '${local.ansible_control_node_cert}' '${local.ansible_control_node_url}/hooks/provision?host=${local.vm_name}'"
   }
 }
